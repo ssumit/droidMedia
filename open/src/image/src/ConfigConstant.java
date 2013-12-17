51bd6ceb2af1b4af863fb72c5722d5cc4a56be75
ssumit <singhsumitbit@gmail.com>
2013-12-17 18:39:14 +0530

    adds config file reader to load configuration

---
+++ open/src/image/src/ConfigConstant.java
@@ -0,0 +1,6 @@
+class ConfigConstant {
+    private final String DISK_CACHE = "disk_cache";
+    private final String DISK_CACHE_SIZE = "disk_cache_size";
+    private final String MEM_CACHE = "mem_cache";
+    private final String MEM_CACHE_SIZE = "mem_cache_size";
+}
