9ffb5f42cae7bde0349b0e9521b604f965823942
ssumit <singhsumitbit@gmail.com>
2013-12-23 16:24:06 +0530

    changes package paths

--- open/src/image/src/com/example/image/PersistentCache.java
+++
@@ -1,8 +0,0 @@
-package com.example.image;
-
-import android.graphics.Bitmap;
-import com.example.cache.MemCache;
-
-public class PersistentCache extends MemCache<String, Bitmap> {
-    //db: url, file link, date accessed, is_valid
-}
